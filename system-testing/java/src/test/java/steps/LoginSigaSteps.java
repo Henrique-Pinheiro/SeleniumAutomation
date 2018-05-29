584c449544eaa673f7472c891fbdd3f934b3035e
Henrique Pinheiro <com>
2018-05-29 09:17:44 -0300

    Creating a POC on a real aplication Siga

--- system-testing/java/src/test/java/steps/LoginSigaSteps.java
+++ system-testing/java/src/test/java/steps/LoginSigaSteps.java
@@ -1,6 +1,11 @@
 package steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
 import pages.LoginSiga;
 import support.BaseSteps;
 
@@ -11,5 +16,27 @@ public class LoginSigaSteps extends BaseSteps {
 
     @Given("^user would like to log in Siga application$")
     public void userwouldliketologinSigaapplication() {
-        }
+        login.openSiga();
     }
+
+    @Given("^user informs Usuário with value equal \"([^\"]*)\"$")
+    public void userInformsUsuárioWithValueEqual(String value) throws Throwable {
+        login.fillUsuario(value);
+    }
+
+
+    @And("^user informs Senha with value equal \"([^\"]*)\"$")
+    public void userInformsSenhaWithValueEqual(String value) throws Throwable {
+        login.fillSenha(value);
+    }
+
+    @When("^User clicks on Confirmar button$")
+    public void userClicksOnConfirmarButton() throws Throwable {
+        login.clickContinuar();
+    }
+
+    @Then("^user should see fail message with \"([^\"]*)\"$")
+    public void userShouldSeeFailMessageWith(String value) throws Throwable {
+        Assert.assertEquals(value, login.checkMessage());
+    }
+}
